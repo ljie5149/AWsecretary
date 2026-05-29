1e821f48b382db105849f2ed63669fff2c5279d3
jacky761233 <ljie5149@gmail.com>
2026-05-29 17:53:08 +0800

    側邊選單完成

--- AWsecretary/wwwroot/js/site.js
+++ AWsecretary/wwwroot/js/site.js
@@ -1,4 +1,21 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//[cite: 1, 3]
+const toggleDropdown = (dropdown, menu, isOpen) => {
+    dropdown.classList.toggle("open", isOpen);
+    menu.style.height = isOpen ? `${menu.scrollHeight}px` : 0;
+}
 
-// Write your JavaScript code.
+document.querySelectorAll(".dropdown-toggle").forEach(btn => {
+    btn.addEventListener("click", e => {
+        e.preventDefault();
+        const dropdown = e.target.closest(".dropdown-container");
+        const menu = dropdown.querySelector(".dropdown-menu");
+        const isOpen = dropdown.classList.contains("open");
+        toggleDropdown(dropdown, menu, !isOpen);
+    });
+});
+
+document.querySelectorAll(".sidebar-toggler, .sidebar-menu-button").forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelector(".sidebar").classList.toggle("collapsed");
+    });
+});
